18f6289483e49d7296478f317f334cc17875e07c
AndrewHanasiro <andrewkhanasiro@gmail.com>
2023-06-18 21:27:40 -0300

    feat: add back on start

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { CSSProperties } from "react";
+import { CSSProperties, useEffect, useState } from "react";
 
 import { CardNumber, CardSuit } from "@/app/_lib/entities";
 
@@ -13,52 +13,64 @@ type CardProps = {
   isExtended: boolean;
 };
 
-function getBlackOrRed(suit: CardSuit) {
-  switch (suit) {
-    case "S":
-    case "C":
-      return `${style.card} ${style.black}`;
-    case "H":
-    case "D":
-      return `${style.card} ${style.red}`;
-    default:
-      break;
-  }
-}
-
-function getSuitImage(suit: CardSuit) {
-  switch (suit) {
-    case "S":
-      return <>&spades;</>;
-    case "C":
-      return <>&clubs;</>;
-    case "H":
-      return <>&hearts;</>;
-    case "D":
-      return <>&diams;</>;
-    default:
-      break;
-  }
-}
-
 export function Card({ position, number, suit, isExtended }: CardProps) {
+  const [isBack, setIsBack] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsBack(false);
+    }, 1500);
+  }, []);
+
+  function getBlackOrRed(suit: CardSuit) {
+    switch (suit) {
+      case "S":
+      case "C":
+        return `${style.card} ${style.black}`;
+      case "H":
+      case "D":
+        return `${style.card} ${style.red}`;
+      default:
+        break;
+    }
+  }
+
+  function getSuitImage(suit: CardSuit) {
+    const mapped = {
+      S: <>&spades;</>,
+      C: <>&clubs;</>,
+      H: <>&hearts;</>,
+      D: <>&diams;</>,
+    };
+    return mapped[suit];
+  }
+
   const inlineStyle: CSSProperties | undefined = isExtended
     ? undefined
     : {
         marginTop: position > 0 ? "-160px" : 0,
         transform: `translateX(${position * 25}px)`,
       };
-  return (
-    <div className={getBlackOrRed(suit)} style={inlineStyle}>
-      <div className={style.card_left}>
-        <span className={style.card_number}>{number}</span>
-        {getSuitImage(suit)}
+
+  function returnFront() {
+    return (
+      <div className={getBlackOrRed(suit)} style={inlineStyle}>
+        <div className={style.card_left}>
+          <span className={style.card_number}>{number}</span>
+          {getSuitImage(suit)}
+        </div>
+        <div className={style.card_center__suit}>{getSuitImage(suit)}</div>
+        <div className={style.card_right}>
+          <span className={style.card_number}>{number}</span>
+          {getSuitImage(suit)}
+        </div>
       </div>
-      <div className={style.card_center__suit}>{getSuitImage(suit)}</div>
-      <div className={style.card_right}>
-        <span className={style.card_number}>{number}</span>
-        {getSuitImage(suit)}
-      </div>
-    </div>
-  );
+    );
+  }
+
+  function returnBack() {
+    return <div className={style.card_back} style={inlineStyle}></div>;
+  }
+
+  return <>{isBack ? returnBack() : returnFront()}</>;
 }
